5d5151195b3008ccf2d17d9900ec28dc17e854c9
Jose Lawrence Aquino <joselawrenceaquino@syn-172-118-018-014.res.spectrum.com>
2024-06-27 14:38:51 +0800

    Job index done

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,4 @@
+<article
+  {{ $attributes->class(['rounded-md border border-slate-300 p-4 shadow-sm w-100%']) }}>
+  {{ $slot }}
+</article>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::get('/', function () {
+    return redirect()->route('jobs.index');
+});
+
+Route::resource('jobs', JobController::class)->only('index');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
